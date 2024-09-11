18baa03ccfcfc7590913d6ad2d2dc24de36fe18e
Jaswanth Harikrishna <jaswanthkriz@gmail.com>
2024-09-11 23:47:02 +0530

    custom widgets added

--- lib/features/presentation/pages/tab_pages/home_page.dart
+++ lib/features/presentation/pages/tab_pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:re_sell_app/features/presentation/widgets/global/custom_appbar.dart';
 import 'package:re_sell_app/features/presentation/widgets/global/custom_text.dart';
+import 'package:re_sell_app/features/presentation/widgets/global/custom_text_form_field.dart';
 import 'package:re_sell_app/features/presentation/widgets/global/custom_textfield.dart';
 import 'package:re_sell_app/features/utils/sizes/sizes.dart';
 
@@ -10,38 +11,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-        appBar: const CustomeAppbarWidget(
-          leadbool: false,
-          title: 'S W I F T S E L L',
-          treaibool: true,
-          trailingIcon: Icons.card_travel,
+      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+      appBar: const CustomeAppbarWidget(
+        leadbool: false,
+        title: 'S W I F T S E L L',
+        treaibool: true,
+        trailingIcon: Icons.card_travel,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppSizes.sizeTwenty,
+            const CustomeTextFiled(
+              controller: null,
+              hintText: "Search here",
+              icon: Icons.search_outlined,
+            ),
+           const CustomTxtFormField(hintText: "nothing"),
+            AppSizes.sizeTwenty,
+            const CommonText(text: 'Best Sellers'),
+            AppSizes.sizeThirty,
+            Container(
+              height: 200,
+              child: ListView.separated(
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => buildCards(),
+                  separatorBuilder: (context, index) =>
+                      AppSizes.sizeWidgthTwentyfive,
+                  itemCount: 6),
+            )
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppSizes.sizeTwenty,
-              const CustomeTextFiled(
-                  controller: null,
-                  hintText: "Search here",
-                  icon: Icons.search_outlined),
-              AppSizes.sizeTwenty,
-              const CommonText(text: 'Best Sellers'),
-              AppSizes.sizeThirty,
-              Container(
-                height: 200,
-                child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) => buildCards(),
-                    separatorBuilder: (context, index) =>
-                        AppSizes.sizeWidgthTwentyfive,
-                    itemCount: 6),
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 
   Widget buildCards() {
